b9f56cc4cb8739343302e2617f34be0c259b87e0
aiden <aidenscott12@googlemail.com>
2017-05-03 19:36:13 +0100

    Populates database

--- server.rb
+++ server.rb
@@ -6,29 +6,23 @@ set :database, {adapter: "sqlite3", database: "foo.sqlite3"}
 
 
 get '/visits' do
-	content_type :json
-	{
-		:visits => {:MAR => {
-			:name => "Marble Arch",
-			:visit_count => 6
-		}, :WAT => {
-			:name => "Waterloo",
-			:visit_count => 10
-		}},
-		:total_visits => 16
-	}.to_json
+  content_type :json
+  {
+      :visits => {:MAR => {
+          :name => "Marble Arch",
+          :visit_count => 6
+      }, :WAT => {
+          :name => "Waterloo",
+          :visit_count => 10
+      }},
+      :total_visits => 16
+  }.to_json
 end
 
 post '/visits' do
-	request.body.rewind
-	request_payload = JSON.parse(request.body.read, :symbolize_names => true)
-	request_payload[:selected_station].to_json
-end
-
-
-get '/record' do
-	Stations.new(:code => "WAT", :name => "Waterloo").save()
-	Stations.all.to_json
+  request.body.rewind
+  request_payload = JSON.parse(request.body.read, :symbolize_names => true)
+  request_payload[:selected_station].to_json
 end
 
 class Visits < ActiveRecord::Base
@@ -38,4 +32,12 @@ end
 class Stations < ActiveRecord::Base
 
 
+end
+
+
+def populate
+  hash = JSON.parse(File.read('tfl-tube-data.json'), :symbolize_names => true)
+  hash[:stations].each do |station, key|
+    Stations.new(:code => key, :name => station).save
+  end
 end
