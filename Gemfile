a8c9fa4b92b04b5c8edaa6ec90c5d48350cd6c7a
aiden <aidenscott12@googlemail.com>
2017-05-03 20:23:24 +0100

    Stations list endpoint

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'mongoid'
 gem 'sinatra-contrib'
+gem 'sinatra-cross_origin'
 
 gem "sinatra-activerecord"
 gem "sqlite3"
--- server.rb
+++ server.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'json'
 require 'sinatra/activerecord'
+require 'sinatra/cross_origin'
+register Sinatra::CrossOrigin
+
+
+
+configure do
+  enable :cross_origin
+  set :allow_origin, :any
+end
 
 set :database, {adapter: "sqlite3", database: "foo.sqlite3"}
 
@@ -19,11 +28,17 @@ get '/visits' do
   }.to_json
 end
 
+get '/stations' do
+  Stations.all.to_json
+end
+
 post '/visits' do
   request.body.rewind
   request_payload = JSON.parse(request.body.read, :symbolize_names => true)
   request_payload[:selected_station].to_json
 end
+
+
 
 class Visits < ActiveRecord::Base
 
@@ -37,7 +52,7 @@ end
 
 def populate
   hash = JSON.parse(File.read('tfl-tube-data.json'), :symbolize_names => true)
-  hash[:stations].each do |station, key|
+  hash[:stations].each do |key, station|
     Stations.new(:code => key, :name => station).save
   end
 end
